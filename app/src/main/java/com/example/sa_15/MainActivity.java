6b882c04ecc3c55720f95ba0dff0571cc6aa3519
Ashwaganha-cader <max.cacz@yandex.ru>
2022-08-02 22:47:07 +0500

    ...

--- app/src/main/java/com/example/sa_15/MainActivity.java
+++ app/src/main/java/com/example/sa_15/MainActivity.java
@@ -2,13 +2,52 @@ package com.example.sa_15;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView first;
+    private TextView second;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        first = findViewById(R.id.tv_first);
+        second = findViewById(R.id.tv_second);
+
+        registerForContextMenu(first);
+        registerForContextMenu(second);
+
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+
+
+        if (v.getId() == R.id.tv_first) {
+
+            menu.add(0);
+            menu.add("for");
+            menu.add("door");
+            menu.add("bor");
+
+
+        }
+
+        else {
+
+            menu.add(0);
+            menu.add("zakd");
+            menu.add("romb");
+            menu.add("park");
+
+        }
+
     }
 }
---
+++ app/src/main/java/com/example/sa_15/OtherBazar.java
@@ -0,0 +1,12 @@
+package com.example.sa_15;
+
+public class OtherBazar {
+
+    public static void main(String[] args) {
+
+        System.out.println("aza_bazar");
+
+    }
+
+
+}
